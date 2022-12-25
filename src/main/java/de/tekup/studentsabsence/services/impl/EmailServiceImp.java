46ca809b1cf9b47512450493417c8f1b2344686b
Rahma <rahmasassi12335@gmail.com>
2022-12-25 19:23:23 +0100

    update

---
+++ src/main/java/de/tekup/studentsabsence/services/impl/EmailServiceImp.java
@@ -0,0 +1,22 @@
+package de.tekup.studentsabsence.services.impl;
+
+import de.tekup.studentsabsence.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImp implements EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(body);
+        javaMailSender.send(mailMessage);
+    }
+
+}
